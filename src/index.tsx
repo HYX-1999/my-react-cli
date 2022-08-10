789280ed1e56be418003d1f56d40532650a3087b
heyuxin <william@rantion.com>
2022-08-10 15:39:23 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+/*
+ * @Description:
+ * @Author: hyx
+ * @Date: 2022-08-09 16:59:24
+ */
+
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "@/redux/store"
+import App from "@/views/App"
+
+const container = document.getElementById("root")
+if (container) {
+  const root = createRoot(container)
+  root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
